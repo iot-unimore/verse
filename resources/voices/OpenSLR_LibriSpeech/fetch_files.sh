ceb98473eed26bf6c187f5e64694e5c4a0c8fa96
Gianluca Filippini <gianluca@autosburla.com>
2025-05-10 15:58:29 +0200

    wip on openslr fetch

--- resources/voices/OpenSLR_LibriSpeech/fetch_files.sh
+++ resources/voices/OpenSLR_LibriSpeech/fetch_files.sh
@@ -5,6 +5,15 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 #
 ########################### DO NOT MODIFY BELOW THIS LINE ##########################################
 
+# English        mls_english_opus.tar.gz    (651G)
+# German         mls_german_opus.tar.gz      (29G)
+# Dutch 	 mls_dutch_opus.tar.gz       (23G)
+# French         mls_french_opus.tar.gz      (16G)
+# Spanish        mls_spanish_opus.tar.gz     (14G)
+# Italian        mls_italian_opus.tar.gz    (3.8G)
+# Portuguese     mls_portuguese_opus.tar.gz (2.5G)
+# Polish         mls_polish_opus.tar.gz     (1.6G)
+
 
 RESOURCES_TAR_SERVER=https://dl.fbaipublicfiles.com/mls
 
@@ -47,9 +56,10 @@ if [ -d  $SCRIPT_DIR/files ] ; then
     RESOURCES_TAR_LINK=`echo $RESOURCES_TAR_SERVER/$i`
     RESOURCES_TAR_FILE=$i
 
-    #echo $RESOURCES_TAR_LINK
-
-    $WGET $RESOURCES_TAR_LINK -a ./wget_fetch.log
+    if [ ! -f ./$RESOURCES_TAR_FILE ] ; then
+       #$WGET $RESOURCES_TAR_LINK -a ./wget_fetch.log
+       $WGET $RESOURCES_TAR_LINK
+    fi
 
     if [ ! -f ./$RESOURCES_TAR_FILE ] ; then
        echo "Error: could not fetch the remote file: $RESOURCES_TAR_FILE"
@@ -57,11 +67,13 @@ if [ -d  $SCRIPT_DIR/files ] ; then
 
     if [ -f ./$RESOURCES_TAR_FILE ] ; then
   	 echo "EXTRACTING FILE: $i"
-         $TAR -xvf $i 2>> tar_extract.log
+         #$TAR -xvf $i 2>> tar_extract.log
+         $TAR -xvf $i
          rm -rf ./$RESOURCES_TAR_FILE
     fi
 
   done
+
 fi
 
 #rm -rf ./wget_fetch.log
