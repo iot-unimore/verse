aaf47df11652e06b54733d96a4eb3c1620a9ac8f
Gianluca Filippini <gianluca@autosburla.com>
2025-09-25 15:33:50 +0200

    update fetch file for unimore rooms

--- resources/rooms/unimore/fetch_files.sh
+++ resources/rooms/unimore/fetch_files.sh
@@ -5,6 +5,45 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 #
 ########################### DO NOT MODIFY BELOW THIS LINE ##########################################
 
-# nothing to fetch for this resource
+RESOURCES_TAR_FILE_LIST=( "verse_unimore_room-003_files_20250925.tar" "verse_unimore_room-004_files_20250925.tar" )
+
+RESOURCES_README_LIST=( "room_003/readme.txt" "room_004/readme.txt")
+
+WGET=`which wget`
+
+cd $SCRIPT_DIR
+
+idx=0
+for i in ${RESOURCES_TAR_FILE_LIST[@]}; do
+
+    RESOURCES_README=${RESOURCES_README_LIST[$idx]}
+
+    RESOURCES_TAR_FILE=$i
+    RESOURCES_TAR_LINK="http://www.brainworks.it/$RESOURCES_TAR_FILE"
+
+    if [ ! -f  $SCRIPT_DIR/files/$RESOURCES_README ] ; then
+      $WGET $RESOURCES_TAR_LINK
+
+      if [ ! -f ./$RESOURCES_TAR_FILE ] ; then
+        echo "Error: could not fetch the remote file: $RESOURCES_TAR_LINK"
+        exit 0
+      fi
+
+      echo "extracting files.."
+      tar -xvf $RESOURCES_TAR_FILE > ./error.log
+
+      if [ ! -f  $SCRIPT_DIR/files/$RESOURCES_README ] ; then
+        echo "Error: could not extract files from: $RESOURCES_TAR_LINK, see error.log"
+        exit 0
+      fi
+
+    fi
+
+    rm -rf ./$RESOURCES_TAR_FILE
+    rm -rf ./error.log
+
+    idx=$((idx+1))
+done
+
 
 echo "done."
---
+++ resources/rooms/unimore/files/room_002/readme.txt
@@ -0,0 +1,4 @@
+BRIR for wilsonClean (no hair on the subject,  no hats, clean head ) with a window of 300ms.
+
+anechoic direct path has been removed as per 3DTuneIn specs.
+
