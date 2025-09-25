0ee68d838f8bfabcf77accb5f2ecb3cf396ef1ee
Gianluca Filippini <gianluca@autosburla.com>
2025-09-25 15:21:35 +0200

    update fetch script for resources/heads/unimore

--- resources/heads/unimore/fetch_files.sh
+++ resources/heads/unimore/fetch_files.sh
@@ -5,35 +5,45 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 #
 ########################### DO NOT MODIFY BELOW THIS LINE ##########################################
 
-RESOURCES_TAR_FILE="verse_unimore_head-003_files_20250924.tar"
+RESOURCES_TAR_FILE_LIST=( "verse_unimore_head-003_files_20250924.tar" "verse_unimore_head-005_files_20250925.tar" "verse_unimore_head-006_files_20250925.tar" "verse_unimore_head-007_files_20250925.tar" "verse_unimore_head-008_files_20250925.tar")
 
-RESOURCES_TAR_LINK="http://www.brainworks.it/$RESOURCES_TAR_FILE"
-
-RESOURCES_README="head_003/readme.txt"
+RESOURCES_README_LIST=( "head_003/readme.txt" "head_005/readme.txt" "head_006/readme.txt" "head_007/readme.txt" "head_008/readme.txt")
 
 WGET=`which wget`
 
 cd $SCRIPT_DIR
 
-if [ ! -f  $SCRIPT_DIR/files/$RESOURCES_README ] ; then
-  $WGET $RESOURCES_TAR_LINK
+idx=0
+for i in ${RESOURCES_TAR_FILE_LIST[@]}; do
 
-  if [ ! -f ./$RESOURCES_TAR_FILE ] ; then
-    echo "Error: could not fetch the remote file: $RESOURCES_TAR_LINK"
-    exit 0
-  fi    
+    RESOURCES_README=${RESOURCES_README_LIST[$idx]}
 
-  echo "extracting files.."
-  tar -xvf $RESOURCES_TAR_FILE > ./error.log
+    RESOURCES_TAR_FILE=$i
+    RESOURCES_TAR_LINK="http://www.brainworks.it/$RESOURCES_TAR_FILE"
 
-  if [ ! -f  $SCRIPT_DIR/files/$RESOURCES_README ] ; then
-    echo "Error: could not extract files from: $RESOURCES_TAR_LINK, see error.log"
-    exit 0
-  fi    
+    if [ ! -f  $SCRIPT_DIR/files/$RESOURCES_README ] ; then
+      $WGET $RESOURCES_TAR_LINK
 
-fi
+      if [ ! -f ./$RESOURCES_TAR_FILE ] ; then
+        echo "Error: could not fetch the remote file: $RESOURCES_TAR_LINK"
+        exit 0
+      fi    
 
-rm -rf ./$RESOURCES_TAR_FILE
-rm -rf ./error.log
+      echo "extracting files.."
+      tar -xvf $RESOURCES_TAR_FILE > ./error.log
+
+      if [ ! -f  $SCRIPT_DIR/files/$RESOURCES_README ] ; then
+        echo "Error: could not extract files from: $RESOURCES_TAR_LINK, see error.log"
+        exit 0
+      fi    
+
+    fi
+
+    rm -rf ./$RESOURCES_TAR_FILE
+    rm -rf ./error.log
+
+    idx=$((idx+1))
+done
+
 
 echo "done."
