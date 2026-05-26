e81829d632e681d8e5fb029f2c068a63710378b5
Gianluca Filippini <gianluca@autosburla.com>
2026-05-26 19:09:37 +0200

    update fetch file for unimore voices

--- resources/voices/unimore/fetch_files.sh
+++ resources/voices/unimore/fetch_files.sh
@@ -5,6 +5,45 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 #
 ########################### DO NOT MODIFY BELOW THIS LINE ##########################################
 
-# nothing to fetch for this resource
+RESOURCES_TAR_FILE="unimore_daps.tar.gz"
+
+RESOURCES_TAR_LINK="http://www.brainworks.it/${RESOURCES_TAR_FILE}"
+
+WGET=`which wget`
+
+cd $SCRIPT_DIR
+
+if [ ! -d  $SCRIPT_DIR/files ] ; then
+  mkdir $SCRIPT_DIR/files
+fi
+
+if [ ! -d  $SCRIPT_DIR/files ] ; then
+   echo "Error: could not create files folder"
+   exit 0
+fi
+
+if [ -d  $SCRIPT_DIR/files ] ; then
+
+  cd $SCRIPT_DIR/files
+
+  $WGET $RESOURCES_TAR_LINK
+
+  if [ ! -f ./$RESOURCES_TAR_FILE ] ; then
+    echo "Error: could not fetch the remote file: $RESOURCES_TAR_LINK"
+    exit 0
+  fi
+
+  echo "extracting files.."
+  tar -xvf $RESOURCES_TAR_FILE > ./error.log
+
+  if [ ! -d  $SCRIPT_DIR/files ] ; then
+    echo "Error: could not extract files from: $RESOURCES_TAR_LINK, see error.log"
+    exit 0
+  fi
+
+fi
+
+#rm -rf ./$RESOURCES_TAR_FILE
+#rm -rf ./error.log
 
 echo "done."
