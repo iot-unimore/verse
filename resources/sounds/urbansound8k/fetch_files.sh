e7353e35ed25e2d6c1f5791f6ac38037ed636154
Gianluca Filippini <gianluca@autosburla.com>
2026-07-08 16:15:03 +0200

    adding urbansound8k for non-human sounds dataset. tbd the info selection to be used in verse

---
+++ resources/sounds/urbansound8k/fetch_files.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+
+#
+########################### DO NOT MODIFY BELOW THIS LINE ##########################################
+
+RESOURCES_TAR_FILE="UrbanSound8K.tar.gz"
+
+RESOURCES_TAR_LINK="https://zenodo.org/record/1203745/files/${RESOURCES_TAR_FILE}"
+
+WGET=`which wget`
+
+cd $SCRIPT_DIR
+
+if [ ! -d  $SCRIPT_DIR/files ] ; then
+  mkdir $SCRIPT_DIR/files
+fi
+
+if [ ! -d  $SCRIPT_DIR/files ] ; then
+   echo "Error: could not create files folder"
+   exit 0
+fi
+
+if [ -d  $SCRIPT_DIR/files ] ; then
+
+  cd $SCRIPT_DIR/files
+
+  $WGET $RESOURCES_TAR_LINK
+
+  if [ ! -f ./$RESOURCES_TAR_FILE ] ; then
+    echo "Error: could not fetch the remote file: $RESOURCES_TAR_LINK"
+    exit 1
+  fi
+
+  echo "extracting files.."
+  unzip $RESOURCES_TAR_FILE > ./error.log
+
+  if [ ! -d  $SCRIPT_DIR/files ] ; then
+    echo "Error: could not extract files from: $RESOURCES_TAR_LINK, see error.log"
+    exit 1
+  fi
+
+  rm -rf ./$RESOURCES_TAR_FILE
+
+fi
+
+echo "done."
---
+++ resources/sounds/urbansound8k/info/readme.txt
@@ -0,0 +1,1 @@
+pleace here info yaml files for AID resources pre-selected for VERSE audio usage.
