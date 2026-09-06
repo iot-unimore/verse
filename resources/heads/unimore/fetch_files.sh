8920ac2dc6b99823b985b7ad80d86a408bea14c3
Gianluca Filippini <gianluca@autosburla.com>
2026-09-06 17:09:06 +0200

    adding two heads reference files, with and without the dummy head in the middle

--- resources/heads/unimore/fetch_files.sh
+++ resources/heads/unimore/fetch_files.sh
@@ -5,9 +5,9 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 #
 ########################### DO NOT MODIFY BELOW THIS LINE ##########################################
 
-RESOURCES_TAR_FILE_LIST=( "verse_unimore_head-003_files_20250924.tar" "verse_unimore_head-005_files_20250925.tar" "verse_unimore_head-006_files_20250925.tar" "verse_unimore_head-007_files_20250925.tar" "verse_unimore_head-008_files_20250925.tar")
+RESOURCES_TAR_FILE_LIST=( "verse_unimore_head-003_files_20250924.tar" "verse_unimore_head-005_files_20250925.tar" "verse_unimore_head-006_files_20250925.tar" "verse_unimore_head-007_files_20250925.tar" "verse_unimore_head-008_files_20250925.tar" "verse_unimore_head-009_files_20260906.tar" "verse_unimore_head-010_files_20260906.tar" "verse_unimore_head-011_files_20260906.tar" "verse_unimore_head-012_files_20260906.tar")
 
-RESOURCES_README_LIST=( "head_003/readme.txt" "head_005/readme.txt" "head_006/readme.txt" "head_007/readme.txt" "head_008/readme.txt")
+RESOURCES_README_LIST=( "head_003/readme.txt" "head_005/readme.txt" "head_006/readme.txt" "head_007/readme.txt" "head_008/readme.txt" "head_009/readme.txt" "head_010/readme.txt" "head_011/readme.txt" "head_012/readme.txt")
 
 WGET=`which wget`
 
